cd7bce0066dc5d7c704d3d6ced00929dd6a44e3a
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2026-05-15 10:26:15 +0530

    feat: update index.html title and add Google Fonts; enhance index.css with theme variables and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Screenshots from './components/Screenshots';
+import Privacy from './components/Privacy';
+import DownloadSection from './components/Download';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className='text-blue-700'>App</div>
-  )
+    <div className="relative">
+      <Navbar />
+      <main>
+        <Hero />
+        <Features />
+        <Screenshots />
+        <Privacy />
+        <DownloadSection />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
